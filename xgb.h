c6ca4815aaf95e1b7719863715ba10cf8165439d
manfouoridanoselatsa <102903836+manfouoridanoselatsa@users.noreply.github.com>
2024-09-24 15:26:50 +0100

    Update xgb.h

--- xgb.h
+++ xgb.h
@@ -38,36 +38,44 @@ class TreeGenerator{
 		int col_count; 
 		double subsample_cols;
 		std::vector<double>  gradient;
-	    	std::vector<double> hessian;
-	    	int min_leaf;
-	    	int num_threads;
-	    	int depth;
-	    	int choice;
-	    	std::vector<int> idxs;
-	    	int min_child_weight;
-	    	std::vector<int> lhsF;
+		std::vector<double> hessian;
+		int min_leaf;
+		int num_threads;
+		int depth;
+		int choice;
+		std::vector<int> idxs;
+		int min_child_weight;
+		std::vector<int> lhsF;
 		std::vector<int> rhsF;
-	    	
-	    	std::vector<std::vector<double>> x;
-	    	TreeGenerator(std::vector<std::vector<double>>& x, std::vector<double>&  gradient,std::vector<double>& hessian,std::vector<int>&  idxs, double subsample_cols, int min_leaf,int min_child_weight ,int depth,double lambda,double gamma,int num_thread,int choice);
-	    	void find_greedy_split_par(int node_idx);
-	    	void find_greedy_split_seq(int node_idx);
-	    	void find_greedy_split_par_feat(int node_idx);
-			void find_greedy_split_par_feat_plus_SplitPoint(int node_idx);
+	    double eps;
+		std::vector<std::vector<double>> x;
+		TreeGenerator(std::vector<std::vector<double>>& x, std::vector<double>&  gradient,std::vector<double>& hessian,std::vector<int>&  idxs, double subsample_cols, int min_leaf,int min_child_weight ,int depth,double lambda,double gamma,double eps,int num_thread,int choice);
+		void find_greedy_split_par(int node_idx);
+		void find_greedy_split_seq(int node_idx);
+		void find_greedy_split_seq_with_na(int node_idx);
+		void find_greedy_split_par_feat(int node_idx);
+		void find_greedy_split_par_node(int node_idx);
+		void find_greedy_split_par_feat_plus_SplitPoint(int node_idx);
+		void find_greedy_split_seq_approx_quantile_sketch(int node_idx);
+		void find_greedy_split_greedy_approximation(int node_idx);
 
-	    	std::vector<int> col_subsample(bool random_col);
-	    	double compute_gamma( std::vector<double>& gradient, std::vector<double>& hessian);
-	    	void find_varsplit_par();
-	    	void find_varsplit_seq();
-	    	void find_varsplit_par_feat();
-			void find_varsplit_par_feat_plus_SplitPoint();
-	    	double gain(std::vector<bool>& lhs,std::vector<bool>& rhs,int node_idx);
-	    	std::vector<double> predict(std::vector<std::vector<double>>& x);
-	    	double predict_row(std::vector<double>& xi);
-	    	void printNode();
-	    	//TreeGenerator &operator=(const TreeGenerator &p);	
-    	
-    		~TreeGenerator() {};
+		std::vector<int> col_subsample(bool random_col);
+		double compute_node_output( std::vector<double>& gradient, std::vector<double>& hessian);
+		void find_varsplit_par();
+		void find_varsplit_seq();
+		void find_varsplit_par_feat();
+		void find_varsplit_par_node();
+		void find_varsplit_seq_with_na();
+		void find_varsplit_par_feat_plus_SplitPoint();
+		void find_varsplit_seq_approx_quantile_sketch();
+		void find_varsplit_greedy_approximation();
+		double gain(std::vector<bool>& lhs,std::vector<bool>& rhs,int node_idx);
+		std::vector<double> predict(std::vector<std::vector<double>>& x);
+		double predict_row(std::vector<double>& xi);
+		void printNode();
+		//TreeGenerator &operator=(const TreeGenerator &p);	
+	
+		~TreeGenerator() {};
 };
 
 class XGBoostClassifier{
@@ -80,6 +88,7 @@ class XGBoostClassifier{
 	   int min_child_weight;
 	   int depth ;
 	   int min_leaf;
+	   int num_threads;
 	   double learning_rate;
 	   int trees;
 	   double lambda;
@@ -91,7 +100,7 @@ class XGBoostClassifier{
 		void grad_hess(std::vector<double>& preds,std::vector<int>& labels,std::vector<double> & grads,std::vector<double> & hess);
 		double log_odds(std::vector<int> labels);
 		void observation_subsample(std::vector<std::vector<double>>& x,std::vector<int>& y,double subsample_ratio,bool random_sample);
-		std::vector<double> fit(std::vector<std::vector<double>>& x, std::vector<int>& y,double subsample_cols, int min_child_weight, int depth,int min_leaf,double learning_rate, int trees,double lambda, double gamma,int num_threads,int choice);
+		std::vector<double> fit(std::vector<std::vector<double>>& x, std::vector<int>& y,double subsample_cols, int min_child_weight, int depth,int min_leaf,double learning_rate, int trees,double lambda, double gamma,double eps,int num_threads,int choice);
 		std::vector<double> predict_proba(std::vector<std::vector<double>> x);
 		std::vector<int> predict(std::vector<std::vector<double>> x);
 		~XGBoostClassifier() {};
